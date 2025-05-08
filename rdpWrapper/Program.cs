e6e609263cb1144c47f05941c7594d6874593bea
Sergiy Egoshyn <egoshin.sergey@gmail.com>
2025-05-08 23:59:08 +0300

    remove single app instance check to allow run in diff sessions of the same user

--- rdpWrapper/Program.cs
+++ rdpWrapper/Program.cs
@@ -1,23 +1,15 @@
 ﻿using sergiye.Common;
 using System;
-using System.Threading;
 using System.Windows.Forms;
 
 namespace rdpWrapper {
   internal static class Program {
-
-    static readonly Mutex mutex = new(true, "{1D73AD65-1407-462C-AD0A-A5938F2FD9BB}");
 
     [STAThread]
     static void Main() {
 
       if (!VersionCompatibility.IsCompatible()) {
         MessageBox.Show("The application is not compatible with your region.", Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Warning);
-        Environment.Exit(0);
-      }
-
-      if (!mutex.WaitOne(TimeSpan.Zero, true)) {
-        MessageBox.Show("Another instance of the application is already running.", Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Information);
         Environment.Exit(0);
       }
 
@@ -30,7 +22,6 @@ namespace rdpWrapper {
         Application.Exit();
       };
       Application.Run(form);
-      mutex.ReleaseMutex();
     }
   }
 }
