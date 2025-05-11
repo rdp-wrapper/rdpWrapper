e6776a60bd92df8391d1b77b26c04c75faa9d9c9
Sergiy Egoshyn <egoshin.sergey@gmail.com>
2025-05-11 14:41:49 +0300

    common submodule replaces with separate repository

--- rdpWrapper/Program.cs
+++ rdpWrapper/Program.cs
@@ -10,7 +10,7 @@ namespace rdpWrapper {
     [STAThread]
     static void Main() {
 
-      if (!VersionCompatibility.IsCompatible()) {
+      if (!OperatingSystemHelper.IsCompatible()) {
         MessageBox.Show("The application is not compatible with your region.", Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Warning);
         Environment.Exit(0);
       }
