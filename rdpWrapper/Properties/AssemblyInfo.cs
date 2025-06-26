ef5fab675a19de383403b88a5d48066a462e7c92
Sergiy Egoshyn <egoshin.sergey@gmail.com>
2025-06-26 13:15:50 +0300

    repository transferred to sergiye

--- rdpWrapper/Properties/AssemblyInfo.cs
+++ rdpWrapper/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("RDP Wrapper")]
 [assembly: AssemblyDescription("RDP Wrapper setup and configuration utility")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("rdp-wrapper")]
+[assembly: AssemblyCompany("SergiyE")]
 [assembly: AssemblyProduct("RDPWrapper")]
 [assembly: AssemblyCopyright("Copyright © Sergiy Egoshyn 2025")]
 [assembly: AssemblyTrademark("")]
